6071cc377a69c40d9146f8eb1caf4fb8a6658e3d
xym <xymfonii@lunant.net>
2010-09-02 20:35:15 +0900

    Added setup.py

---
+++ lessipy/tree/numeric.py
@@ -0,0 +1,80 @@
+import numbers
+from less.tree.operator import Operand
+
+
+def numeric(cls):
+    """Gets numeric value of a given object."""
+    try:
+        return cls.__numeric__()
+    except AttributeError:
+        if isinstance(cls, numbers.Number):
+            return cls
+    raise ValueError("cls must be numeric, passed " + repr(cls))
+
+
+class Numeric(Operand):
+    """A numerical."""
+
+    def __init__(self, val):
+        """Creates a number.
+        
+        :param val: a numeric value. (e.g 1, 1.0, 0x22)
+
+        """
+        self.val = val
+        self.unit = None
+
+    def __numeric__(self):
+        """Returns numeric value"""
+        return numeric(self.val)
+
+    def to_css(self):
+        return str(self.val)
+
+    def unify(self, other):
+        """Unifies `Numeric` or `Measure` unit."""
+        try:
+            if not self.unit or not other.unit:
+                return self.unit if self.unit else other.unit
+            elif self.unit == other.unit:
+                return self.unit
+            raise ArithmeticError("It is impossible to operate "  \
+                                 + repr(self.unit) + " and " + repr(other.unit))
+        except AttributeError:
+            raise ArithmeticError("It is impossible to operate "  \
+                                  + repr(self) + " and " + repr(other))
+
+    def basic(self, other, __method__):
+        """The basic operation method"""
+        lval = self.evaluate()
+        rval = other.evaluate()
+        unit = self.unify(rval)
+        result = getattr(float(numeric(lval)), __method__)(float(numeric(rval)))
+        if unit:
+            return Measure(Numeric(result), unit)
+        return self.__class__(Numeric(result), unit)
+
+
+class Measure(Numeric):
+    """A `Numeric` and unit pair(e.g 1px, 2em, 100%, 3pt, ...)"""
+
+    def __init__(self, val, unit):
+        """Creates a pair.
+        
+        :param val: a `Numeric` instance or `int`.
+        :param unit: an unit. (e.g px, pt, em, %, ...)
+
+        """
+        if isinstance(val, numbers.Number):
+            val = Numeric(val)
+        if not isinstance(val, Numeric):
+            raise ValueError("val must `Numeric`, passed " + repr(val))
+        if unit == '%':
+            self.val = val.val / 100.0
+            self.unit = None
+        else:
+            self.val = val
+            self.unit = unit
+
+    def to_css(self):
+        return "{val}{unit}".format(val=self.val.to_css(), unit=self.unit)
---
+++ setup.py
@@ -0,0 +1,30 @@
+"""
+Lessipy
+-------
+"""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name="Lessipy",
+    version="0.0.1",
+    license="public",
+    author="xymz",
+    author_email="",
+    description="",
+    long_description=__doc__,
+    packages=["lessipy"],
+    platforms="any",
+    install_requires=[
+        "PIL"
+    ],
+    test_suite="test",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console"
+    ]
+)
+
