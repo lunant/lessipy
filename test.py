ebff8581d1f2ae13fbb72b3060f97c858361ce84
xym <xymfonii@lunant.net>
2010-09-02 23:36:53 +0900

    Added setup scripts

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import doctest
-from less import *
+from lessipy import *
 
 doctest.testfile("tests/declaration.txt", optionflags=doctest.REPORT_ONLY_FIRST_FAILURE)
 #doctest.testfile("tests/types.txt", raise_on_error=True)
