cd6dd585e2c2e16b0f131e67bd327b458510903e
xym <xymfonii@lunant.net>
2010-09-07 14:24:18 +0900

    Added valid message

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ import doctest
 from lessipy import *
 
 doctest.testfile("tests/declaration.txt", optionflags=doctest.REPORT_ONLY_FIRST_FAILURE)
+print "test valid"
 #doctest.testfile("tests/types.txt", raise_on_error=True)
 
